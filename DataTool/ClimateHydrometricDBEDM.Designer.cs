4327b1d9e72fae57a4223c268c6efcd0c9a57beb
ECCC-CSSP <charles.leblanc2@canada.ca>
2022-07-08 16:59:20 -0300

    update

--- DataTool/ClimateHydrometricDBEDM.Designer.cs
+++ DataTool/ClimateHydrometricDBEDM.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\CSSP latest code\DataTool\DataTool\ClimateHydrometricDBEDM.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\CSSP Latest Code Old\DataTool\DataTool\ClimateHydrometricDBEDM.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
