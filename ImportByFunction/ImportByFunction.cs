d9abb43d20f435dfe45a796f1e6fc1b55725a0a9
ECCC-CSSP <charles.leblanc2@canada.ca>
2023-05-26 14:18:09 -0300

    Update ImportByFunction.cs

--- ImportByFunction/ImportByFunction.cs
+++ ImportByFunction/ImportByFunction.cs
@@ -20069,7 +20069,7 @@ namespace ImportByFunction
             //sb.AppendLine("Province,SectorID,Sector,Date,h0,h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11,h12,h13,h14,h15,h16,h17,h18,h19,h20,h21,h22,h23,h24,h25,h26,h27,h28,h29,h30");
 
             #region Getting Subsector sites mean lat, lng
-            for (int i = 1; i < 2; i++)
+            for (int i = 0; i < 6; i++)
             {
                 string ProvStr = ProvList[i];
                 string ProvInit = ProvInitList[i];
@@ -20215,7 +20215,7 @@ namespace ImportByFunction
 
                                 nearCoord = coordDist.c;
                                 dist = coordDist.d;
-                                webTideName = "ne-pac4";
+                                webTideName = "ne_pac4";
                             }
                             else if (ProvInit == "BC")
                             {
@@ -20253,6 +20253,7 @@ namespace ImportByFunction
 
                             var MWQMRunList = (from c in db.MWQMRuns
                                                where c.SubsectorTVItemID == TVItemIDSS
+                                               && c.Tide_h0_m == null
                                                orderby c.DateTime_Local
                                                select c).ToList();
 
